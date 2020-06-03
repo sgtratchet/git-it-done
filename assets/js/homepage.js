7d11ea0eaa391a1a66a5e708f494b41ccaf227e6
Devin ellsworth <ellsworths@Sabrinas-Mac-mini.local>
2020-06-02 23:02:00 -0600

    started 6.1.6

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ var getUserRepos = function(){
     fetch("https://api.github.com/users/octocat/repos").then(function(response) {
         response.json().then(function(data) {
         console.log(data);
-  });
-});
-
+        });
+    });
+}
 getUserRepos();
